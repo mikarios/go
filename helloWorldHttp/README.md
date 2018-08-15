cfb1e8b366411250f7bf6fafc17be532541827c0
mikarios <manos.ikarios@gmail.com>
2018-08-16 02:34:25 +0300

    Improvements on logging added authentication parameters can be passed as flags to executable

--- helloWorldHttp/README.md
+++ helloWorldHttp/README.md
@@ -1,2 +1,24 @@
 # helloWorldHttp
 A simple http server for me to remember the capabilities.
+Variables:
+* ListeningPort
+* Username
+* Password
+* UsernameQueryParam
+* UsernameHeadParam
+* PasswordQueryParam
+* PasswordHeadParam
+can be set on compilation:
+```Bash
+go build -ldflags "-X main.Username=newUsername -X main.Password=newPassword -X ListeningPort=80 -X main.UsernameQueryParam=u -X main.UsernameHeadParam=u -X main.PasswordQueryParam=p -X main.PasswordHeadParam=p" helloWorld.go
+```
+or can be passed as arguments:
+```Bash
+./helloWorld -port 8081 --usernameQueryParam=user --passwordQueryParam pass
+```
+
+#### logging http.Handler
+Logs every request
+ 
+#### authenticate http.Handler
+Checks all Username and Password Parameters for accepted values; Ignores favicon.ico
--- helloWorldHttp/helloWorld.go
+++ helloWorldHttp/helloWorld.go
@@ -2,47 +2,121 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	. "github.com/logrusorgru/aurora"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"time"
 )
 
+type Logger struct {
+	*log.Logger
+}
+
 var (
-	logger        = log.New(os.Stdout, "http: ", log.LstdFlags)
-	ListeningPort = "8080"
+	logger             = &Logger{log.New(os.Stdout, "http: ", log.LstdFlags)}
+	ListeningPort      = flag.Int("port", 8080, "port number")
+	Username           = "user"
+	Password           = "pass"
+	UsernameQueryParam = flag.String("usernameQueryParam", "u", "What the username query param will be.")
+	UsernameHeadParam  = flag.String("usernameHeadParam", "u", "What the username header will be.")
+	PasswordQueryParam = flag.String("passwordQueryParam", "p", "What the password query param will be.")
+	PasswordHeadParam  = flag.String("passwordHeadParam", "p", "What the password header will be.")
+	OutputMode         = flag.String("outputMode", "debug", "Accepted values: debug, info, warning, error")
 )
+
+const (
+	OUTPUT_DEBUG   = "debug"
+	OUTPUT_INFO    = "info"
+	OUTPUT_WARNING = "warning"
+	OUTPUT_ERROR   = "error"
+)
+
+func isDebugModeOn() bool {
+	return *OutputMode == OUTPUT_DEBUG
+}
+
+func isInfoModeOn() bool {
+	return *OutputMode == OUTPUT_DEBUG || *OutputMode == OUTPUT_INFO
+}
+
+func isWarningModeOn() bool {
+	return *OutputMode != OUTPUT_ERROR
+}
+
+func (logger *Logger) debug(a ...interface{}) {
+	if isDebugModeOn() {
+		logger.Println(Brown("Debug:"), Brown(a))
+	}
+}
+
+func (logger *Logger) success(a ...interface{}) {
+	if isInfoModeOn() {
+		logger.Println(Green("Success:"), Green(a))
+	}
+}
+
+func (logger *Logger) info(a ...interface{}) {
+	if isInfoModeOn() {
+		logger.Println("Info:", a)
+	}
+}
+
+func (logger *Logger) warn(a ...interface{}) {
+	if isWarningModeOn() {
+		logger.Println(Magenta("Warning"), Magenta(a))
+	}
+}
+
+func (logger *Logger) error(a ...interface{}) {
+	if isWarningModeOn() {
+		logger.Print(Red("Error"), Red(a))
+		os.Exit(-1)
+	}
+}
 
 func helloWorldServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "The page you requested ("+r.URL.Path[1:]+") says Hello!")
 }
 
+// Used for logging each request
 func logging() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer logger.Println("Request " + r.URL.String() + " answered")
-			logger.Println(r.RemoteAddr + " requested " + r.URL.String())
+			defer logger.debug("Request " + r.URL.String() + " answered")
+			logger.debug(r.RemoteAddr + " requested " + r.URL.String())
 			next.ServeHTTP(w, r)
 		})
 	}
 }
-func checkForPron(h http.Handler) http.Handler {
+
+func authenticate(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Println("CheckingforPron")
-		if strings.Contains(r.URL.String(), "pron") {
-			w.WriteHeader(404)
-			fmt.Fprintln(w, "You will not find pron here!")
+		//Just show the favicon, who cares?
+		if strings.Contains(r.URL.Path, "favicon.ico") {
+			h.ServeHTTP(w, r)
 			return
 		}
+		logger.debug("Checking if user is authenticated")
+		if !(r.Header.Get(*PasswordHeadParam) == Password || r.URL.Query().Get(*PasswordQueryParam) == Password) ||
+			!(r.Header.Get(*UsernameHeadParam) == Username || r.URL.Query().Get(*UsernameQueryParam) == Username) {
+			w.WriteHeader(401)
+			fmt.Fprintln(w, "You are not authorized to be here!")
+			logger.warn("Unauthorized request", r.Header, r.Body, r.URL)
+			return
+		}
+		logger.debug("Authenticated successfully")
 		h.ServeHTTP(w, r)
 	})
 }
 
 //If we didn't want to dynamically call the next handler we could:
-//Don't forget to set server.Handler to : checkForPron(router)
+//Don't forget to set server.Handler to : authenticate(router)
 //func logging(h http.Handler) http.Handler {
 //	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 //		defer logger.Println("Request " + r.URL.String() + " answered")
@@ -51,8 +125,8 @@ func checkForPron(h http.Handler) http.Handler {
 //	})
 //}
 //
-//func checkForPron(h http.Handler) http.Handler {
-//	checkforpron := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//func authenticate(h http.Handler) http.Handler {
+//	authenticate := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 //		logger.Println("CheckingforPron")
 //		if strings.Contains(r.URL.String(), "pron") {
 //			w.WriteHeader(404)
@@ -65,22 +139,30 @@ func checkForPron(h http.Handler) http.Handler {
 //}
 
 func main() {
-	logger.Println("Server is starting...")
+	flag.Parse()
+
+	*OutputMode = strings.ToLower(*OutputMode)
+
+	if *OutputMode != OUTPUT_DEBUG && *OutputMode != OUTPUT_INFO && *OutputMode != OUTPUT_WARNING && *OutputMode != OUTPUT_ERROR {
+		logger.error("No valid output mode was defined:", *OutputMode)
+	}
+
+	logger.info("Server is starting...")
 	//Set up all the routing we need
 	router := http.NewServeMux()
 	router.HandleFunc("/", helloWorldServer)
 	fs := http.FileServer(http.Dir("helloWorldHttp/public/"))
 	router.Handle("/favicon.ico", fs)
 	//Adding our Middleware
-	//h1 := checkForPron(router)
+	//h1 := authenticate(router)
 	//hhs := logging()(h1)
 
 	//Defining custom attributes for the server
 	//If we wanted the default ones we could go with http.ListenAndServe(":"+ListeningPort,h1)
 	server := &http.Server{
-		Addr:         ":" + ListeningPort,
-		Handler:      logging()(checkForPron(router)),
-		ErrorLog:     logger,
+		Addr:         ":" + strconv.Itoa(*ListeningPort),
+		Handler:      logging()(authenticate(router)),
+		ErrorLog:     logger.Logger,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  15 * time.Second,
@@ -91,19 +173,19 @@ func main() {
 	go func() {
 		//Shutting down the server
 		<-quit
-		logger.Println("Server is shutting down...")
+		logger.info("Server is shutting down...")
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 		server.SetKeepAlivesEnabled(false)
 		if err := server.Shutdown(ctx); err != nil {
-			logger.Fatal("Could not gracefully shutdown the server: " + err.Error() + "\n")
+			logger.error("Could not gracefully shutdown the server: ", err.Error())
 		}
 		close(done)
 	}()
-	logger.Println("Server is ready to handle requests at", server.Addr)
+	logger.info("Server is ready to handle requests at", server.Addr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatal("Could not listen on :" + ListeningPort + ". Error: " + err.Error())
+		logger.error("Could not listen on :", *ListeningPort, "Error: ", err.Error())
 	}
 	<-done
-	logger.Println("Server stopped")
+	logger.info("Server stopped")
 }
